d0bf45fb1fadedc51e6b820170e47e9adba4485d
MariiaBatyuta <mashabatyuta.work@gmail.com>
2024-08-22 23:41:10 +0200

    deleted redux part

--- src/redux/mailing/selectors.ts
+++
@@ -1,5 +0,0 @@
-import { RootState } from "../store";
-
-export const selectorMailingSuccess = (state: RootState) => state.contact.isSended;
-export const selectorMailingSendingNow = (state: RootState) => state.contact.isSendingNow;
-export const selectorMailingError = (state: RootState) => state.contact.isError;
