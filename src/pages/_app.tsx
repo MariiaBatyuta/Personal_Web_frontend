35c3fe90f1459f5dfb8602a72b276aba400d213e
MariiaBatyuta <mashabatyuta.work@gmail.com>
2024-08-23 22:05:01 +0200

    add lazy loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,42 +1,25 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import Layout from '@/layouts/Layouts';
 import '@/styles/global.css';
 import { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import dynamic from 'next/dynamic';
 import Loader from '@/components/Loader/Loader';
 
 const queryClient = new QueryClient();
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const handleStart = () => {
-      setIsLoading(true);
-    };
-
-    const handleComplete = () => {
-      setIsLoading(false);
-    };
-
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleComplete);
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleComplete);
-      router.events.off('routeChangeError', handleComplete);
-    };
-  }, [router.events]);
+export default function MyApp({ Component, pageProps, router }: AppProps) {
+  const LazyComponent = dynamic(() => Promise.resolve(Component), {
+    ssr: false,
+    loading: () => <Loader />,
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
-      <Layout>{isLoading ? <Loader /> : <Component {...pageProps} />}</Layout>
+      <Layout>
+        <LazyComponent {...pageProps} />
+      </Layout>
     </QueryClientProvider>
   );
 }
