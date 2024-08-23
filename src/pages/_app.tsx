e70083921ba16157af4e05f4449d8e3d6b3c7fbf
MariiaBatyuta <mashabatyuta.work@gmail.com>
2024-08-23 21:42:53 +0200

    changed router events
    Transition when page changed.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Layout from '@/layouts/Layouts';
 import '@/styles/global.css';
 import { AppProps } from 'next/app';
@@ -8,20 +9,40 @@ import { AnimatePresence, motion } from 'framer-motion';
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => setIsTransitioning(true);
+    const handleComplete = () =>
+      setTimeout(() => setIsTransitioning(false), 300);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router.events]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <Toaster />
       <Layout>
         <AnimatePresence mode="wait">
-          <motion.div
-            key={router.route}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Component {...pageProps} />
-          </motion.div>
+          {!isTransitioning && (
+            <motion.div
+              key={router.route}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.4 }}
+            >
+              <Component {...pageProps} />
+            </motion.div>
+          )}
         </AnimatePresence>
       </Layout>
     </QueryClientProvider>
